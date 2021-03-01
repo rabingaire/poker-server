48a8449821829c971214bc45c5a2e6dcd8893b48
Subash Poudel <blogsubash@gmail.com>
2021-03-01 13:11:01 +0545

    socket-io: Sample chat server implemented

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import './env';
 import './db';
 
-import fs from 'fs';
-import cors from 'cors';
-import path from 'path';
-import helmet from 'helmet';
-import morgan from 'morgan';
 import express from 'express';
-import favicon from 'serve-favicon';
-import bodyParser from 'body-parser';
-import compression from 'compression';
 import * as Sentry from '@sentry/node';
 
 import routes from './routes';
-import json from './middlewares/json';
-import logger, { logStream } from './utils/logger';
-import * as errorHandler from './middlewares/errorHandler';
 import SocketIO from 'socket.io';
 import http from 'http';
-import gameSocket from './gameSocket';
 
 // Initialize Sentry
 // https://docs.sentry.io/platforms/node/express/
@@ -30,7 +18,6 @@ const APP_PORT =
   (process.env.NODE_ENV === 'test' ? process.env.TEST_APP_PORT : process.env.APP_PORT) || process.env.PORT || '3000';
 const APP_HOST = process.env.APP_HOST || '0.0.0.0';
 
-const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
 
 app.set('port', APP_PORT);
 app.set('host', APP_HOST);
@@ -38,77 +25,22 @@ app.set('host', APP_HOST);
 app.locals.title = process.env.APP_NAME;
 app.locals.version = process.env.APP_VERSION;
 
-// This request handler must be the first middleware on the app
-app.use(Sentry.Handlers.requestHandler());
-
-app.use(favicon(path.join(__dirname, '/../public', 'favicon.ico')));
-app.use(cors());
-app.use(helmet());
-app.use(compression());
-app.use(morgan('tiny', { stream: logStream }));
-app.use(bodyParser.json());
-app.use(errorHandler.bodyParser);
-app.use(json);
-
-// API Routes
-app.use('/api', routes);
-
-// Swagger UI
-// Workaround for changing the default URL in swagger.json
-// https://github.com/swagger-api/swagger-ui/issues/4624
-const swaggerIndexContent = fs
-  .readFileSync(`${pathToSwaggerUi}/index.html`)
-  .toString()
-  .replace('https://petstore.swagger.io/v2/swagger.json', '/api/swagger.json');
-
-app.get('/api-docs/index.html', (req, res) => res.send(swaggerIndexContent));
-app.get('/api-docs', (req, res) => res.redirect('/api-docs/index.html'));
-app.use('/api-docs', express.static(pathToSwaggerUi));
-
-// This error handler must be before any other error middleware
-app.use(Sentry.Handlers.errorHandler());
-
-// Error Middleware
-app.use(errorHandler.genericErrorHandler);
-app.use(errorHandler.methodNotAllowed);
-
-app.listen(app.get('port'), app.get('host'), () => {
-  logger.info(`Server started at http://${app.get('host')}:${app.get('port')}/api`);
-});
-
-// Catch unhandled rejections
-process.on('unhandledRejection', err => {
-  logger.error('Unhandled rejection', err);
-
-  try {
-    Sentry.captureException(err);
-  } catch (err) {
-    logger.error('Sentry error', err);
-  } finally {
-    process.exit(1);
-  }
-});
-
-// Catch uncaught exceptions
-process.on('uncaughtException', err => {
-  logger.error('Uncaught exception', err);
-
-  try {
-    Sentry.captureException(err);
-  } catch (err) {
-    logger.error('Sentry error', err);
-  } finally {
-    process.exit(1);
-  }
-});
-
 const server = http.Server(app);
-const io = SocketIO(server, {path: "/game"});
 
-// io.on('connection', socket => gameSocket(socket));
-io.on('connection', socket => {
-  console.log('A user has connected', socket);
-  socket.emit("chat message", "Some thing to show");
+const io = SocketIO(server);
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(APP_PORT, () => {
+  console.log('listening on *:' + APP_PORT);
 });
 
 export default app;
