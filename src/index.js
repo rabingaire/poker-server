a664bf5f4361fff398db615df927145218c43a0d
Subash Poudel <blogsubash@gmail.com>
2021-03-01 15:57:54 +0545

    main: Minor change for heroku

--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ import SocketIO from 'socket.io';
 import http from 'http';
 import gameSocket from './gameSocket';
 
-
 const app = express();
 
 const APP_PORT =
   (process.env.NODE_ENV === 'test' ? process.env.TEST_APP_PORT : process.env.APP_PORT) || process.env.PORT || '3000';
 const APP_HOST = process.env.APP_HOST || '0.0.0.0';
-
 
 app.set('port', APP_PORT);
 app.set('host', APP_HOST);
@@ -27,6 +25,7 @@ const io = SocketIO(server);
 io.on('connection', (socket) => gameSocket(socket, io));
 
 app.get('/', (req, res) => {
+  console.log('requesting web page');
   res.sendFile(__dirname + '/index.html');
 });
 
